9d4ad6ab70eb3151f8244896bf115a1ead950913
martinbob1992 <610385220@qq.com>
2022-05-18 14:06:36 +0800

    Update run.sh

--- java程序docker化/run.sh
+++ java程序docker化/run.sh
@@ -9,6 +9,8 @@ set -e
 docker build --build-arg SPRING_BOOT_YML="-Dspring.profiles.active=test -Dserver.port=$port -Dmarsh.log.path=/opt/java/logs" -t $project:latest -f $projectPath/build/Dockerfile $projectPath/build
 set +e
 docker rm -f $project
+# 清理悬空镜像
+docker rmi $(docker images -q -f dangling=true)
 set -e
 # -v路径根据实际情况修改
 docker run -d --name $project --restart=always -p $port:$port \
